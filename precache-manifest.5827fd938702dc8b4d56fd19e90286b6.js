4eac74b58a42b242de36f18758b6e07d634e4311
= <=>
2020-07-26 20:00:30 +0530

    Updates

---
+++ precache-manifest.5827fd938702dc8b4d56fd19e90286b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aefe454ea40d841ee09a3c38a3862d3d",
+    "url": "/Monsters-Place/index.html"
+  },
+  {
+    "revision": "c8f09e6431b046f720c6",
+    "url": "/Monsters-Place/static/css/main.937fceec.chunk.css"
+  },
+  {
+    "revision": "cc86f92a0e9b1ed7b6f7",
+    "url": "/Monsters-Place/static/js/2.1b35cb52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Place/static/js/2.1b35cb52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8f09e6431b046f720c6",
+    "url": "/Monsters-Place/static/js/main.31e161e2.chunk.js"
+  },
+  {
+    "revision": "3415ac9e75941589bf54",
+    "url": "/Monsters-Place/static/js/runtime-main.227004ec.js"
+  }
+]);
